e0eab6150784bfed8df798b1a8f17fe0e0a0b2bf
cardles <cardles.dev@gmail.com>
2021-09-04 17:22:34 -0300

    update name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
-let DOOR = process.env.DOOR || 8080;
+let PORT = process.env.PORT || 8080;
 
-app.listen(DOOR, () => {
-    console.log(`Servidor rodando com sucesso na porta ${DOOR}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando com sucesso na porta ${PORT}`);
 })
