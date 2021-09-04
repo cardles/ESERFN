7172fc5381627b5e9af93e8da1bc94ee981c1a94
cardles <cardles.dev@gmail.com>
2021-09-04 14:20:07 -0300

    correcting cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
 
 const db = require("./data/databaseConnection"); 
 db.connect();
 
-const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 
 const indexRoutes = require("./routes/indexRoutes");
