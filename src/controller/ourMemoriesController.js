874740a0b85c0626fef8c512ae8252cf201a7537
cardles <cardles.dev@gmail.com>
2021-09-09 18:49:27 -0300

    testing connection with database

--- src/controller/ourMemoriesController.js
+++ src/controller/ourMemoriesController.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 
 
 const getAllMemories = async (req, res) => {
-    const allMemories = await Memories.find()
-    res.status(200).json(allMemories)
+    try {
+        const allMemories = await Memories.find()
+        res.status(200).json(allMemories)
+    } catch (err) {
+        res.status(500).json({ message: "me tacaro balinha fudeu" })
+    }
 };
 
 const getMemoriesByCity = (req, res) => {
--- src/data/databaseConnection.js
+++ src/data/databaseConnection.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGODB = process.env.MONGODB_URI || process.env.MONGODB_URL
+const MONGODB = process.env.MONGODB_URI
+// process.env.MONGODB_URL ||
 
 const connect = () => {
     mongoose.connect(MONGODB, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Database conectada com sucesso!"))
-    .catch(err => console.err)
+    .then(() => console.log("Database conectada com sucesso!"))
+    .catch(err => console.log(err))
 };
 
 
